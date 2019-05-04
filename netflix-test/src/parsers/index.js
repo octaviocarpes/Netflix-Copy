bec31f97afbe956822418354c4e3047f4c12c18f
Octavio <octavio.carpes10@gmail.com>
2019-05-04 09:45:45 -0300

    Added vuex implementation for videos

---
+++ netflix-test/src/parsers/index.js
@@ -0,0 +1,14 @@
+import _ from "lodash"
+
+export default {
+  parseVideos ({ items }) {
+    return items.map(item => {
+      return {
+        id: _.get(item, "id.videoId", ""),
+        title: _.get(item, "snippet.title", ""),
+        description: _.get(item, "snippet.description", ""),
+        thumbnails: _.get(item, "snippet.thumbnails", "")
+      }
+    })
+  }
+}
