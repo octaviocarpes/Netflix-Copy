d2011507dc2698d46f87682794a08b27af645291
Octavio <octavio.carpes10@gmail.com>
2019-05-04 23:03:42 -0300

    Finished Player View

--- netflix-test/src/views/player/Player.vue
+++ netflix-test/src/views/player/Player.vue
@@ -1,9 +1,68 @@
 <template>
-  <h1>Player</h1>
+  <div class="player-container">
+    <div class="back-to-movies" @click="goToMovies()">
+      <img :src="require(`../../assets/img/back-arrow.svg`)" alt="back icon">
+      <p>Back to Movies</p>
+    </div>
+    <iframe
+      :src="movieURL"
+      frameborder="0"
+      width="100%"
+      height="100%"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Player",
+
+  computed: {
+    movieURL() {
+      return `https://www.youtube.com/embed/${this.$route.params.id}?autoplay=1`
+    }
+  },
+
+  methods: {
+    goToMovies() {
+      this.$router.push("/movies");
+    }
+  }
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.player-container {
+  height: 100%;
+  width: 100%;
+
+  .back-to-movies {
+    
+    display: flex;
+
+    position: absolute;
+    z-index: 999;
+
+    img {
+      width: 100px;
+      height: 100px;
+
+      padding-top: 35px;
+    }
+
+    p {
+      padding-top: 76px;
+      color: white;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
+</style>
