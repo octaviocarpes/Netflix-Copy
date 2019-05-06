233730668256bdf14d121d63c00392056f6f87d0
Octavio <octavio.carpes10@gmail.com>
2019-05-05 23:35:40 -0300

    Added new API KEY

--- netflix-test/config/config.dev.js
+++ netflix-test/config/config.dev.js
@@ -1,5 +1,6 @@
 // I needed 2 API Keys because of the youtube api quota limit.
 export default {
   // API_KEY: "AIzaSyAlFhJEK-B_bUDjfyUiK8pdR7eRHJsK3_g"
-  API_KEY: "AIzaSyBDFW6_iuJSqCfiCkTCsXBA2G36Ol7MsRQ"
+  // API_KEY: "AIzaSyBDFW6_iuJSqCfiCkTCsXBA2G36Ol7MsRQ"
+  API_KEY: "AIzaSyAveTKXose8res_eQl_eE6EWWlYT8ZEOgM"
 };
